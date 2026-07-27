0935cb540787407ce4ea9c768bfa36e8f1470f8f
Gary Gregory <garydgregory@gmail.com>
2026-07-27 15:17:59 +0000

    Better use of JUnit API (comment).

--- src/test/java/org/apache/commons/net/ntp/TestTimeInfo.java
+++ src/test/java/org/apache/commons/net/ntp/TestTimeInfo.java
@@ -87,7 +87,7 @@ class TestTimeInfo {
         assertEquals(info, other); // fails
         assertEquals(info.hashCode(), other.hashCode());
         other.addComment("another comment");
-        // assertFalse(info.equals(other)); // comments not used for equality
+        // assertNotEquals(info, other); // comments not used for equality
 
         final TimeInfo another = new TimeInfo(packet, returnTime, new ArrayList<>());
         assertEquals(info, another);
